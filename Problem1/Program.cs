4f61f5daf5af109808b42c0ebb4361258bb10faa
Galin <Galin@DESKTOP-CAL3NOT>
2024-04-02 13:14:59 +0300

    Add project files.

---
+++ Problem1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Problem1
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, long> resources = new Dictionary<string, long>();
+
+            while (true)
+            {
+                string resource = Console.ReadLine();
+
+                if (resource == null || resource == "")
+                    break;
+
+                long quantity = long.Parse(Console.ReadLine());
+
+                if (resources.ContainsKey(resource))
+                {
+                    resources[resource] += quantity;
+                }
+                else
+                {
+                    resources[resource] = quantity;
+                }
+            }
+
+            foreach (var kvp in resources)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
---
+++ Problem2/Program.cs
@@ -0,0 +1,23 @@
+﻿Dictionary<string, int> resources = new Dictionary<string, int>();
+
+string resource = Console.ReadLine();
+
+while (resource != "stop")
+{
+    int quantity = int.Parse(Console.ReadLine());
+
+    if (resources.ContainsKey(resource))
+    {
+        resources[resource] += quantity;
+    }
+    else
+    {
+        resources.Add(resource, quantity);
+    }
+resource = Console.ReadLine();
+}
+
+foreach (var pair in resources)
+{
+    Console.WriteLine($"{pair.Key} -> {pair.Value}");
+}
